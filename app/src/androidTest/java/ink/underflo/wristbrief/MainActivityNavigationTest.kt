2dbe8750f77b98e3577bb1d88abef8b1f545d24a
Xihao Zhang <zeromostia@gmail.com>
2026-09-11 11:23:12 +0800

    Fix Wear Compose test matcher import

--- app/src/androidTest/java/ink/underflo/wristbrief/MainActivityNavigationTest.kt
+++ app/src/androidTest/java/ink/underflo/wristbrief/MainActivityNavigationTest.kt
@@ -3,7 +3,6 @@ package ink.underflo.wristbrief
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
